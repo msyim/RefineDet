12d01e8e84f3ce377f693385a6b35d75915285ed
Minsub Yim <minsub@Minsubs-MacBook-Pro.local>
2020-03-01 23:30:03 +0900

    ODM and TCB modules added

--- modules/ARM.py
+++ modules/ARM.py
@@ -13,21 +13,17 @@ class ARM(nn.Module):
         self.conv5_l2norm = L2NormLayer(512, 10)
 
         # FC6 and FC7 in VGG are converted into conv layers
-        self.conv_fc6 = nn.Sequential(
-            nn.Conv2d(512,  1024, kernel_size=3, padding=3, dilation=3),
-            nn.ReLU()
-        )
         self.conv_fc7 = nn.Sequential(
+            nn.Conv2d(512,  1024, kernel_size=3, padding=3, dilation=3),
+            nn.ReLU(),
             nn.Conv2d(1024, 1024, kernel_size=1),
             nn.ReLU()
         )
 
         # Extra layers(conv6_1 and conv6_2) added to capture high-level information
-        self.conv6_1  = nn.Sequential(
-            nn.Conv2d(1024, 256,  kernel_size=1),
-            nn.ReLU()
-        )
         self.conv6_2  = nn.Sequential(
+            nn.Conv2d(1024, 256,  kernel_size=1),
+            nn.ReLU(),
             nn.Conv2d(256,  512,  kernel_size=3, stride=2, padding=1),
             nn.ReLU()
         )
@@ -35,44 +31,41 @@ class ARM(nn.Module):
         # Filters to predict ARM loc and conf. scores
         self.conv4_3_loc  = nn.Conv2d(512, num_bbox * 4, kernel_size=3, padding=1)
         self.conv5_3_loc  = nn.Conv2d(512, num_bbox * 4, kernel_size=3, padding=1)
-        self.conv6_1_loc  = nn.Conv2d(1024, num_bbox * 4, kernel_size=3, padding=1)
-        self.conv6_2_loc  = nn.Conv2d(1024, num_bbox * 4, kernel_size=3, padding=1)
+        self.convfc7_loc  = nn.Conv2d(1024, num_bbox * 4, kernel_size=3, padding=1)
+        self.conv6_2_loc  = nn.Conv2d(512, num_bbox * 4, kernel_size=3, padding=1)
 
         self.conv4_3_conf = nn.Conv2d(512, num_bbox * 2, kernel_size=3, padding=1)
         self.conv5_3_conf = nn.Conv2d(512, num_bbox * 2, kernel_size=3, padding=1)
-        self.conv6_1_conf = nn.Conv2d(1024, num_bbox * 2, kernel_size=3, padding=1)
-        self.conv6_2_conf = nn.Conv2d(1024, num_bbox * 2, kernel_size=3, padding=1)
+        self.convfc7_conf = nn.Conv2d(1024, num_bbox * 2, kernel_size=3, padding=1)
+        self.conv6_2_conf = nn.Conv2d(512, num_bbox * 2, kernel_size=3, padding=1)
 
     def forward(self, conv4_3, conv5_3):
-
-        # Apply up to conv_fc7
-        conv_fc7 = self.conv_fc7(self.conv_fc6(self.mp(conv5_3)))
 
         # The following 4 feature maps will be used in training
         # 1. L2Normed conv4_3
         # 2. L2Normed conv5_3
-        # 3. conv6_1 (one of the two extra layers to capture high-level information)
-        # 4. conv6_2 (one of the two extra layers to capture high-level information)
+        # 3. conv_fc7
+        # 5. conv6_2 (one of the two extra layers to capture high-level information)
         l2_conv4_3 = self.conv4_l2norm(conv4_3)
         l2_conv5_3 = self.conv5_l2norm(conv5_3)
-        conv6_1    = self.conv6_1(conv_fc7)
-        conv6_2    = self.conv6_2(conv6_1)
+        conv_fc7   = self.conv_fc7(self.mp(conv5_3))
+        conv6_2    = self.conv6_2(conv_fc7)
 
         # Apply ARM loc
         conv4_3_loc = self.conv4_3_loc(l2_conv4_3)
         conv5_3_loc = self.conv5_3_loc(l2_conv5_3)
-        conv6_1_loc = self.conv6_1_loc(conv6_1)
+        convfc7_loc = self.convfc7_loc(conv_fc7)
         conv6_2_loc = self.conv6_2_loc(conv6_2)
 
         # Apply ARM conf
         conv4_3_conf = self.conv4_3_conf(l2_conv4_3)
         conv5_3_conf = self.conv5_3_conf(l2_conv5_3)
-        conv6_1_conf = self.conv6_1_conf(conv6_1)
+        convfc7_conf = self.conv6_1_conf(conv_fc7)
         conv6_2_conf = self.conv6_2_conf(conv6_2)
 
-        featmap_list = [l2_conv4_3, l2_conv5_3, conv6_1, conv6_2]
-        armloc_list  = [conv4_3_loc, conv5_3_loc, conv6_1_loc, conv6_2_loc]
-        armconf_list = [conv4_3_conf, conv5_3_conf, conv6_1_conf, conv6_2_conf]
+        featmap_list = [l2_conv4_3, l2_conv5_3, conv_fc7, conv6_2]
+        armloc_list  = [conv4_3_loc, conv5_3_loc, convfc7_loc, conv6_2_loc]
+        armconf_list = [conv4_3_conf, conv5_3_conf, convfc7_conf, conv6_2_conf]
 
         return featmap_list, armloc_list, armconf_list
 
--- modules/TCB.py
+++ modules/TCB.py
@@ -1,11 +1,51 @@
 import torch.nn as nn
 
+def feature_scale(input_channel):
+    layer = nn.Sequential(
+        nn.Conv2d(input_channel, 256, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(256, 256, kernel_size=3, padding=1)
+    )
+    return layer
+
+def pred_layer():
+    layer = nn.Sequential(
+        nn.ReLU(),
+        nn.Conv2d(256, 256, kernel_size=3, padding=1),
+        nn.ReLU()
+    )
+    return layer
+
 class TCB(nn.Module):
-    def __init__(self):
+    def __init__(self, channel_sizes):
         super(TCB, self).__init__()
 
-    def forward(x):
-        return x
+        self.feat_scales = nn.ModuleList([ feature_scale(cs) for cs in channel_sizes ])
+        self.pred_layers = nn.ModuleList([ pred_layer() for _ in channel_sizes ])
+        self.upsamples   = nn.ModuleList([ nn.ConvTranspose2d(256, 256, 2, 2) for _ in channel_sizes[:1] ])
+
+    def forward(self, featmap_list):
+
+        # apply feature scale layer to all 4 of the feature maps
+        scaled     = [ self.feat_scales[i](featmap) for i, featmap in enumerate(featmap_list) ]
+
+        # upsample the last 3 of the feature scaled outputs
+        upsampled  = [ self.upsamples[i](s) for i, s in enumerate(scaled[1:]) ]
+
+        # apply element-wise sum to the upsampled with the first 3 of feature scaled
+        elwise_sum = [ scaled[i] + upsampled[i] for i in upsampled ]
+
+        # append the last element of scaled to elwise_sum list
+        elwise_sum.append(scaled[-1])
+
+        # apply pred layer to the elementwise sum
+        TCB_outputs = [ self.pred_layers(i)[s] for i,s in enumerate(elwise_sum) ]
+
+        return TCB_outputs
+
+
+
+
 
 
 

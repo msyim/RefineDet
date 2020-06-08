10364ae3f8d751bdfd9fbf2bc727ec381411d399
msyim <msyim826@gmail.com>
2020-06-08 09:25:54 +0900

    scheduler updated to tune lr according to t_loss

--- train.py
+++ train.py
@@ -12,21 +12,28 @@ from RefineDet import refine_det
 import json
 import sys
 
+torch.autograd.set_detect_anomaly(True)
+
 conf = json.loads(open('conf.json').read())
 bbox_conf  = conf['bbox_conf']
 train_conf = conf['train_conf']
+dset_conf  = conf['dataset_conf']
 backbone = VGG16()
 model = refine_det(backbone, conf).cuda()
 #model.weight_initialize()
 print(model)
 
-dataset = VOCDetection(root='../../data/VOCdevkit/', transform=SSDAugmentation(512))
-testset = VOCDetection('../../data/VOCdevkit/', [('2007', 'test')],
+if train_conf['trained_weights'] != '':
+    model.load_state_dict(torch.load(train_conf['trained_weights']))
+
+dataset = VOCDetection(root=dset_conf['dataset_root'], transform=SSDAugmentation(512))
+testset = VOCDetection(dset_conf['dataset_root'], [('2007', 'test')],
                            BaseTransform(bbox_conf['image_size'], mean = (104, 117, 123)),
                            VOCAnnotationTransform())
-dl = data.DataLoader(dataset = dataset, collate_fn=detection_collate, batch_size=12, shuffle=True)
+dl = data.DataLoader(dataset = dataset, collate_fn=detection_collate, batch_size=13, shuffle=True)
 criterion = MultiBoxLoss(conf)
 optimizer = torch.optim.SGD(params = model.parameters(), momentum=train_conf['momentum'], weight_decay = train_conf['weight_decay'], lr = train_conf['lr'])
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True, eps=1e-8)
 
 max_map = 0.0
 
@@ -50,10 +57,10 @@ for epoch in range(train_conf['num_epochs']):
         t_loss += loss.data
         loss.backward()
         optimizer.step()
-        num_itrs += 1
 
         sys.stdout.write("\r[E: %03d][I: %05d] LOSS: %.4f(ALL: %.4f, ACL: %.4f, OLL: %.4f, OCL: %.4f)" 
-                    %(epoch, num_itrs, t_loss/num_itrs, t_all/num_itrs, t_alc/num_itrs, t_oll/num_itrs, t_olc/num_itrs))
+                    %(epoch, num_itrs, t_loss/num_itrs, t_all/num_itrs, t_alc/num_itrs, t_oll/num_itrs, t_olc/num_itrs))    
+        num_itrs += 1
 
     sys.stdout.write('\n')
     model.eval()
@@ -64,5 +71,6 @@ for epoch in range(train_conf['num_epochs']):
             torch.save(model.state_dict(), './saved_models/best_accuracy.pth')
         print('Best MAP: %f, cur MAP: %f' % (max_map, MAP))
     model.train()
+    scheduler.step(t_loss)
 
 
